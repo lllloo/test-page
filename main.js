d902e81090c7e8ee79d17f013c817a2efd0cbedc
lllloo <asdf01618@gmail.com>
2026-03-19 09:32:53 +0000

    feat: 將 Chatbase 腳本移至 main.js，並在各頁面中引用

---
+++ main.js
@@ -0,0 +1,51 @@
+;(function () {
+  if (!window.chatbase || window.chatbase('getState') !== 'initialized') {
+    window.chatbase = (...arguments) => {
+      if (!window.chatbase.q) {
+        window.chatbase.q = []
+      }
+      window.chatbase.q.push(arguments)
+    }
+    window.chatbase = new Proxy(window.chatbase, {
+      get(target, prop) {
+        if (prop === 'q') {
+          return target.q
+        }
+        return (...args) => target(prop, ...args)
+      },
+    })
+  }
+  const onLoad = function () {
+    const script = document.createElement('script')
+    script.src = 'https://www.chatbase.co/embed.min.js'
+    script.id = 'j9Lh2F3IAy8j9DoOjdRZx'
+    script.domain = 'www.chatbase.co'
+    document.body.appendChild(script)
+  }
+  if (document.readyState === 'complete') {
+    onLoad()
+  } else {
+    window.addEventListener('load', onLoad)
+  }
+
+  // Based on current page
+  const currentPage = window.location.pathname;
+  let messages = [];
+
+  if (currentPage.includes('/page-a.html')) {
+    messages = [
+      "這是在A頁面上的訊息。",
+    ];
+  } else if (currentPage.includes('/page-b.html')) {
+    messages = [
+      "這是在B頁面上的訊息。",
+    ];
+  } else {
+    messages = [
+      "這是在首頁上的訊息。",
+    ];
+  }
+
+  window.chatbase.setInitialMessages(messages);
+
+})()
